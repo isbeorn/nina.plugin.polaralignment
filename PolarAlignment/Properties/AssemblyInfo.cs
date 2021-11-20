0270bb0b52a76c9f91ac57ea1106606edf61a72a
Stefan Berg <isbeorn86@googlemail.com>
2021-11-20 02:36:21 +0100

    Version 1.5.3.0

--- PolarAlignment/Properties/AssemblyInfo.cs
+++ PolarAlignment/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 
 //The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("1.5.2.1")]
-[assembly: AssemblyFileVersion("1.5.2.1")]
+[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyFileVersion("1.5.3.0")]
 
 //The minimum Version of N.I.N.A. that this plugin is compatible with
 [assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2001")]
